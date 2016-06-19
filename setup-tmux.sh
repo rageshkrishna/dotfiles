ad4ad138b866718558e6bee396e5daef3a0093de
Ragesh Krishna <ragesh@rageshkrishna.com>
2016-06-19 11:17:43 +0530

    Create setup-tmux.sh

---
+++ setup-tmux.sh
@@ -0,0 +1,3 @@
+pip install --user git+git://github.com/powerline/powerline
+cp tmux-powerline.conf ~/.local/lib/python2.7/site-packages/powerline/bindings/tmux/powerline.conf
+cp tmux.conf ~/.tmux.conf
