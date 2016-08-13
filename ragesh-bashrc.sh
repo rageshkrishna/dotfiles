ed1d59c328329ad5e55da7ce4855a30d910965d4
Ragesh Krishna <ragesh@rageshkrishna.com>
2016-08-13 15:57:33 +0530

    Add authship and proxyship bash aliases

--- ragesh-bashrc.sh
+++ ragesh-bashrc.sh
@@ -15,6 +15,8 @@ fi
 LS_COLORS=$LS_COLORS:'di=01;36:'; export LS_COLORS
 
 alias shgrep='grep --exclude-dir=logs --exclude-dir=node_modules --exclude-dir=.git --exclude=*.min.js --exclude=*.cat.js -Rn'
-alias shmigrate='psql -h 172.17.42.1 -U apiuser -W -d shipdb -f ~/api/models/migrations.sql'
+alias migrateship='psql -h 172.17.42.1 -U apiuser -W -d shipdb -f ~/api/models/migrations.sql'
+alias authship='psql -h 172.17.42.1 -U apiuser -d shipdb -f ~/custom/setupAuth.sql'
+alias proxyship='docker-compose -f ~/rageshkrishna/nginx-dev-proxy/nginx.yml up -d nginx'
 
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[00;36m\]\w$(__git_ps1 " \[\033[38;5;208m\](%s)")\[\033[00m\]\$ '
